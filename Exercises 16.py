a0f3709098718d0622dcc20453b222e412ec47f2
aga-moj-nick <agnieszka.aga.krawczyk@gmail.com>
2021-06-28 10:15:26 +0200

    Tytuł

--- Exercises 16.py
+++ Exercises 16.py
@@ -1,3 +1,5 @@
+﻿# Write a Python program to get the difference between a given number and 17, if the number is greater than 17 return double the absolute difference
+
 a = int (input ("Podaj liczbę a: "))
 odejmowanie = int (a - 17)
 
